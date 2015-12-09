c2c52aa4218d9ebdabb2fd8f9474aef6bf71edcf
Burhanuddin Sunelwala <burhanuddin353@gmail.com>
2015-12-09 13:08:39 +0400

    added methods to create UIColor object with hex value, retrieve hex value or hex string from UIColor.

--- TFTColor/UIColor+TFT.h
+++ TFTColor/UIColor+TFT.h
@@ -12,12 +12,66 @@
 
 @interface UIColor (TFT)
 
-+ (UIColor *)colorWithRGBHex:(NSString *)hexCode;
+///-----------------------------------------------------------
+/// @name RGB Methods
+///-----------------------------------------------------------
 
+/**
+@param hexString RGB Hex String. Can optionally be prefixed with #, 0x or 0X.
+@param alpha The opacity value of the color object, specified as a value from 0.0 to 1.0.
+*/
++ (UIColor *)colorWithRGBHexString:(NSString *)hexString alpha:(CGFloat)alpha;
+
+/**
+ @param hexValue RGB Hex Value. Should be prefixed with 0x.
+ @param alpha The opacity value of the color object, specified as a value from 0.0 to 1.0.
+ */
++ (UIColor *)colorWithRGBHexValue:(NSUInteger)hexValue alpha:(CGFloat)alpha;
+
+/**
+ @return Returns a RGB Hex String for a specified UIColor.
+ */
++ (NSString *)rgbHexStringForColor:(UIColor *)color;
+
+/**
+ @return Returns a RGB Hex Value for a specified UIColor.
+ */
++ (NSUInteger)rgbHexValueForColor:(UIColor *)color;
+
+
+///-----------------------------------------------------------
+/// @name CNYK Methods
+///-----------------------------------------------------------
+
+/**
+ @param cyan The cyan component of the color object, specified as a value from 0.0 to 1.0.
+ @param magenta The magenta component of the color object, specified as a value from 0.0 to 1.0.
+ @param yellow The yellow component of the color object, specified as a value from 0.0 to 1.0.
+ @param black The black component of the color object, specified as a value from 0.0 to 1.0.
+ @param alpha The opacity value of the color object, specified as a value from 0.0 to 1.0.
+ */
 + (UIColor *)colorWithCyan:(CGFloat)cyan magenta:(CGFloat)magenta yellow:(CGFloat)yellow black:(CGFloat)black alpha:(CGFloat)alpha;
 
-+ (UIColor *)colorWithCMYKHex:(NSString *)hexCode;
+/**
+ @param hexString CMYK Hex String. Can optionally be prefixed with #, 0x or 0X.
+ @param alpha The opacity value of the color object, specified as a value from 0.0 to 1.0.
+ */
++ (UIColor *)colorWithCMYKHexString:(NSString *)hexString alpha:(CGFloat)alpha;
 
-+ (NSString *)hexStringForColor:(UIColor *)color;
+/**
+ @param hexValue CMYK Hex Value. Should be prefixed with 0x.
+ @param alpha The opacity value of the color object, specified as a value from 0.0 to 1.0.
+ */
++ (UIColor *)colorWithCMYKHexValue:(NSUInteger)hexValue alpha:(CGFloat)alpha;
+
+/**
+ @return Returns a CMYK Hex String for a specified UIColor.
+ */
++ (NSString *)cmykHexStringForColor:(UIColor *)color;
+
+/**
+ @return Returns a CMYK Hex Value for a specified UIColor.
+ */
++ (NSUInteger)cmykHexValueForColor:(UIColor *)color;
 
 @end
--- TFTColor/UIColor+TFT.m
+++ TFTColor/UIColor+TFT.m
@@ -13,12 +13,14 @@ NSString *const HEX_CHAR_SET = @"abcdefABCDEF1234567890";
 
 @implementation UIColor (TFT)
 
-+ (void)cleanHexString:(NSMutableString *)hexString expectedLength:(int)length {
+#pragma mark - Helper Methods
++ (void)cleanHexString:(NSMutableString *)hexString expectedLength:(int)expectedLength {
     
     if ([hexString hasPrefix:@"#"]) {
         
         [hexString deleteCharactersInRange:NSMakeRange(0, 1)];
-    } else if ([hexString hasPrefix:@"0x"] ||
+    }
+    else if ([hexString hasPrefix:@"0x"] ||
                [hexString hasPrefix:@"0X"]) {
         
         [hexString deleteCharactersInRange:NSMakeRange(0, 2)];
@@ -26,7 +28,6 @@ NSString *const HEX_CHAR_SET = @"abcdefABCDEF1234567890";
     
     //Check for Special Characters. Truncate the string from first special character.
     NSCharacterSet *characterSet = [[NSCharacterSet characterSetWithCharactersInString:HEX_CHAR_SET] invertedSet];
-    
     NSUInteger firstInvalidChar = [hexString rangeOfCharacterFromSet:characterSet].location;
     if (firstInvalidChar != NSNotFound) {
         
@@ -34,49 +35,104 @@ NSString *const HEX_CHAR_SET = @"abcdefABCDEF1234567890";
     }
     
     //Repeat each hex digit if length is half
-    if (length % 2 == 0 && hexString.length == length/2) {
+    if (hexString.length == expectedLength/2) {
         
-        for (int i = 0; i < length/2; ++i) {
+        for (int i = 0; i < expectedLength/2; ++i) {
             
             [hexString insertString:[hexString substringWithRange:NSMakeRange(i*2, 1)] atIndex:i*2];
         }
-    } else if (hexString.length < length) {
+    }
+    //Append zeros if the length is less than the expected length
+    else if (hexString.length < expectedLength) {
         
-        while (hexString.length != length) {
+        while (hexString.length != expectedLength) {
             [hexString insertString:@"0" atIndex:0];
         }
-    } else if(hexString.length > length) {
-         //Truncate the excess string if length is greater than expected length
+    }
+    //Truncate the excess string if length is greater than expected length
+    else if(hexString.length > expectedLength) {
         
-        [hexString deleteCharactersInRange:NSMakeRange(length, hexString.length - length)];
+        [hexString deleteCharactersInRange:NSMakeRange(expectedLength, hexString.length - expectedLength)];
     }
 }
 
-+ (NSArray *)colorComponentsWithHexcode:(NSMutableString *)hexcode components:(int)components {
++ (NSUInteger)hexValueFromHexString:(NSString *)hexString {
     
-    //Actual code converting each two hex digits into int
-    NSMutableArray *colorComponents = [NSMutableArray array];
+    unsigned hexValue = 0;
+    [[NSScanner scannerWithString:hexString] scanHexInt:&hexValue];
+    return hexValue;
+}
 
-    for (int i = 0; i < components * 2; i += 2) {
-        
-        NSString *hex = [hexcode substringWithRange:NSMakeRange(i, 2)];
-        unsigned int color = 0;
-        [[NSScanner scannerWithString:hex] scanHexInt:&color];
-        [colorComponents addObject:[NSNumber numberWithFloat:color / MAX_VAL]];
-    }
++ (NSArray *)rgbComponentsForColor:(UIColor *)color {
+    
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat alpha;
+    [color getRed:&red green:&green blue:&blue alpha:&alpha];
+    
+    NSArray *colorComponents = @[@(red), @(green), @(blue), @(alpha)];
     return colorComponents;
 }
 
-+ (UIColor *)colorWithRGBHex:(NSString *)hexCode {
++ (NSArray *)cmykComponentsForColor:(UIColor *)color {
     
-    NSMutableString *mutableHexCode =  [hexCode mutableCopy];
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat alpha;
+    [color getRed:&red green:&green blue:&blue alpha:&alpha];
     
-    [UIColor cleanHexString:mutableHexCode expectedLength:6];
-    NSArray *color = [self colorComponentsWithHexcode:mutableHexCode components:3];
+    CGFloat black = 1 - MAX(MAX(red, blue), green);
+    CGFloat cyan = (1 - red - black) / (1 - black);
+    CGFloat magenta = (1 - green - black) / (1 - black);
+    CGFloat yellow = (1 - blue - black) / (1 - black);
     
-    return [UIColor colorWithRed:[color[0] floatValue] green:[color[1] floatValue] blue:[color[2] floatValue] alpha:1.0f];
+    NSArray *colorComponents = @[@(cyan), @(magenta), @(yellow), @(black), @(alpha)];
+    return colorComponents;
 }
 
+#pragma mark - Public Methods
+
+#pragma mark RGB
++ (UIColor *)colorWithRGBHexString:(NSString *)hexString alpha:(CGFloat)alpha {
+    
+    NSMutableString *mutableHexCode =  [hexString mutableCopy];
+    [UIColor cleanHexString:mutableHexCode expectedLength:6];
+    return [UIColor colorWithRGBHexValue:[self hexValueFromHexString:mutableHexCode] alpha:alpha];
+}
+
++ (UIColor *)colorWithRGBHexValue:(NSUInteger)hexValue alpha:(CGFloat)alpha {
+
+    return [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0
+                           green:((float)((hexValue & 0xFF00) >> 8))/255.0
+                            blue:((float)(hexValue & 0xFF))/255.0 alpha:1.0];
+}
+
++ (NSString *)rgbHexStringForColor:(UIColor *)color {
+
+    NSArray *rgbComponents = [UIColor rgbComponentsForColor:color];
+    
+    NSMutableString *hexString = [NSMutableString string];
+    [hexString appendFormat:@"%02x", ([rgbComponents[0] unsignedIntValue] * 255)];
+    [hexString appendFormat:@"%02x", ([rgbComponents[1] unsignedIntValue] * 255)];
+    [hexString appendFormat:@"%02x", ([rgbComponents[2] unsignedIntValue] * 255)];
+    
+    return hexString;
+}
+
++ (NSUInteger)rgbHexValueForColor:(UIColor *)color {
+    
+    NSArray *rgbComponents = [UIColor rgbComponentsForColor:color];
+    
+    NSUInteger hexValue = [rgbComponents[0] unsignedIntValue] * 255 * 0xFFFFFF;
+    hexValue += [rgbComponents[1] unsignedIntValue] * 255 * 0xFFFF;
+    hexValue += [rgbComponents[2] unsignedIntValue] * 255 * 0xFF;
+    
+    return hexValue;
+}
+
+#pragma mark CNYK
 + (UIColor *)colorWithCyan:(CGFloat)cyan magenta:(CGFloat)magenta yellow:(CGFloat)yellow black:(CGFloat)black alpha:(CGFloat)alpha {
     
     CGFloat red = (1.0f - cyan) * (1.0f - black);
@@ -86,31 +142,44 @@ NSString *const HEX_CHAR_SET = @"abcdefABCDEF1234567890";
     return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
-+ (UIColor *)colorWithCMYKHex:(NSString *)hexCode {
++ (UIColor *)colorWithCMYKHexValue:(NSUInteger)hexValue alpha:(CGFloat)alpha {
     
-    NSMutableString *mutableHexCode =  [hexCode mutableCopy];
-    
-    [UIColor cleanHexString:mutableHexCode expectedLength:8];
-    NSArray *color = [self colorComponentsWithHexcode:mutableHexCode components:4];
-    
-    return [UIColor colorWithCyan:[color[0] floatValue] magenta:[color[1] floatValue] yellow:[color[2] floatValue] black:[color[3] floatValue] alpha:1.0f];
+    return [UIColor colorWithCyan:((float)((hexValue & 0xFF000000) >> 32))/255.0
+                          magenta:((float)((hexValue & 0xFF0000) >> 16))/255.0
+                           yellow:((float)((hexValue & 0xFF00) >> 8))/255.0
+                            black:((float)(hexValue & 0xFF))/255.0 alpha:alpha];
 }
 
-+ (NSString *)hexStringForColor:(UIColor *)color {
++ (UIColor *)colorWithCMYKHexString:(NSString *)hexString alpha:(CGFloat)alpha {
     
-    CGFloat red;
-    CGFloat green;
-    CGFloat blue;
-    CGFloat alpha;
+    NSMutableString *mutableHexCode =  [hexString mutableCopy];
+    [UIColor cleanHexString:mutableHexCode expectedLength:8];
+    return [UIColor colorWithCMYKHexValue:[self hexValueFromHexString:mutableHexCode] alpha:alpha];
+}
+
++ (NSString *)cmykHexStringForColor:(UIColor *)color {
     
-    [color getRed:&red green:&green blue:&blue alpha:&alpha];
+    NSArray *cmykComponents = [UIColor cmykComponentsForColor:color];
     
     NSMutableString *hexString = [NSMutableString string];
-    [hexString appendFormat:@"%02x", (int)(red * 255)];
-    [hexString appendFormat:@"%02x", (int)(green * 255)];
-    [hexString appendFormat:@"%02x", (int)(blue * 255)];
+    [hexString appendFormat:@"%02x", ([cmykComponents[0] unsignedIntValue] * 255)];
+    [hexString appendFormat:@"%02x", ([cmykComponents[1] unsignedIntValue] * 255)];
+    [hexString appendFormat:@"%02x", ([cmykComponents[2] unsignedIntValue] * 255)];
+    [hexString appendFormat:@"%02x", ([cmykComponents[3] unsignedIntValue] * 255)];
     
     return hexString;
+}
+
++ (NSUInteger)cmykHexValueForColor:(UIColor *)color {
+    
+    NSArray *cmykComponents = [UIColor cmykComponentsForColor:color];
+    
+    NSUInteger hexValue = [cmykComponents[0] unsignedIntValue] * 255 * 0xFFFFFFFF;
+    hexValue += [cmykComponents[1] unsignedIntValue] * 255 * 0xFFFFFF;
+    hexValue += [cmykComponents[2] unsignedIntValue] * 255 * 0xFFFF;
+    hexValue += [cmykComponents[3] unsignedIntValue] * 255 * 0xFF;
+    
+    return hexValue;
 }
 
 @end
