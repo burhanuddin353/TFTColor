b02547439da4e8ee78d62e77a54e0b3094b6f57e
Burhanuddin Sunelwala <burhanuddin353@gmail.com>
2016-08-23 10:49:53 +0400

    remove unwanted files

--- Swift Example/Swift Example/TFTColor.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TFTColor.swift
-//  Swift Example
-//
-//  Created by Burhanuddin Sunelwala on 12/9/15.
-//  Copyright © 2015 Burhanuddin Sunelwala. All rights reserved.
-//
-
-import Foundation
